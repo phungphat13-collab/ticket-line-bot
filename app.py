220faa978a72f8c57f07c4e4d03ce93c2373304e
phungphat13-collab <phungphat13@gmail.com>
2025-11-20 22:46:39 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,21 +1,7 @@
-from flask import Flask, request, abort
-from linebot import LineBotApi, WebhookHandler
-from linebot.exceptions import InvalidSignatureError
-from linebot.models import MessageEvent, TextMessage, TextSendMessage
+from flask import Flask, request
 import os
-import logging
-
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
 
 app = Flask(__name__)
-
-# Environment variables
-channel_secret = os.getenv('LINE_CHANNEL_SECRET')
-channel_access_token = os.getenv('LINE_ACCESS_TOKEN')
-
-line_bot_api = LineBotApi(channel_access_token)
-handler = WebhookHandler(channel_secret)
 
 @app.route("/")
 def home():
@@ -23,28 +9,8 @@ def home():
 
 @app.route("/callback", methods=['POST'])
 def callback():
-    signature = request.headers['X-Line-Signature']
-    body = request.get_data(as_text=True)
-    
-    logger.info(f"📨 Webhook received: {body}")
-    
-    try:
-        handler.handle(body, signature)
-        logger.info("✅ Webhook handled")
-    except InvalidSignatureError:
-        abort(400)
-    
+    print("Webhook received!")
     return 'OK'
-
-@handler.add(MessageEvent, message=TextMessage)
-def handle_message(event):
-    logger.info(f"💬 Message: {event.message.text}")
-    
-    # Reply với tin nhắn nhận được
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text=f"Bot đã nhận: {event.message.text}")
-    )
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
