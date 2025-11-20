e1315e099eb3639d9ab84cab580ece286b180856
phungphat13-collab <phungphat13@gmail.com>
2025-11-20 22:11:37 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
-from flask import Flask
+from flask import Flask, request
+import os
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return "🤖 Bot is running! ✅"
+
+@app.route("/callback", methods=['POST'])
+def callback():
+    print("Webhook received")
+    return 'OK'
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
